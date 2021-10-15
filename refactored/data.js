6e187c445ee2ac34227c79b63e7de07fe9417273
juliusmarminge <51714798+juliusmarminge@users.noreply.github.com>
2021-10-16 00:11:30 +0200

    I decided to refactor this project

---
+++ refactored/data.js
@@ -0,0 +1,51 @@
+export const data = 
+{
+    "1": {
+        question: "Vilken amerikansk delstat är markerad på bilden?",
+        img: "cali.png",
+        option1: "Kalifornien",
+        option2: "San Fransisco",
+        option3: "Las Vegas",
+        answer: "1"
+    },
+    "2": {
+        question: "Vilken amerikansk delstat är markerad på bilden?",
+        img: "maryland.png",
+        option1: "New York",
+        option2: "Washington",
+        option3: "Maryland",
+        answer: "3"
+    },
+    "3": {
+        question: "Vilken amerikansk delstat är markerad på bilden?",
+        img: "colorado.png",
+        option1: "Idaho",
+        option2: "Colorado",
+        option3: "Utah",
+        answer: "2"
+    },
+    "4": {
+        question: "Vilken amerikansk delstat är markerad på bilden?",
+        img: "florida.png",
+        option1: "Beverly Hills",
+        option2: "Miami",
+        option3: "Florida",
+        answer: "3"
+    },
+    "5": {
+        question: "Vilken amerikansk delstat är markerad på bilden?",
+        img: "connecticut.png",
+        option1: "Connecticut",
+        option2: "Rhode Island",
+        option3: "New Jersey",
+        answer: "1"
+    },
+    "6": {
+        question: "Vilken amerikansk delstat är markerad på bilden?",
+        img: "illinois.png",
+        option1: "Minnesota",
+        option2: "Illinois",
+        option3: "Chicago",
+        answer: "2"
+    },
+};
